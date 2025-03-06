6f69f17d478f14a736e216c016abe8789f99d288
Swecle <swecleisnice@gmail.com>
2025-03-06 18:13:21 +0800

    Add files via upload
    I tried making a mini dictionary or translator to test how some syntax work. It is not much though.

---
+++ English_to_Japanese_Translator.py
@@ -0,0 +1,9 @@
+print("English --> Japanese Translator")
+English = input("Translate: ")
+dictionary = {
+    "press": "押す",
+    "enter": "入る",
+    "start": "始める"
+}
+output = ""
+print(dictionary[English])
